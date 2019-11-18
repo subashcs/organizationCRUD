910863ee3a55d15930d3a2fd17caf20a9691631f
subashcs <subashsapkota59@gmail.com>
2019-11-18 11:53:39 +0545

    Updates

---
+++ precache-manifest.18625e384587f378f8e5225df327800f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1e1c7a42bedd461bdc2553a86c20bb",
+    "url": "/organizationCRUD/index.html"
+  },
+  {
+    "revision": "2df466e409f49a1be493",
+    "url": "/organizationCRUD/static/css/2.bd4f2bc1.chunk.css"
+  },
+  {
+    "revision": "d2a0609ff9f71fd83d6b",
+    "url": "/organizationCRUD/static/css/main.25a5fbd6.chunk.css"
+  },
+  {
+    "revision": "2df466e409f49a1be493",
+    "url": "/organizationCRUD/static/js/2.c74f8d29.chunk.js"
+  },
+  {
+    "revision": "d2a0609ff9f71fd83d6b",
+    "url": "/organizationCRUD/static/js/main.9c5427a3.chunk.js"
+  },
+  {
+    "revision": "2ee3e651efdae93df20b",
+    "url": "/organizationCRUD/static/js/runtime-main.ea0085c5.js"
+  }
+]);
